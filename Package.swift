59cde76a8813fb277dee915876cda560e2024b6f
Alexandre H. Saad <alexandresaad@icloud.com>
2022-06-09 19:40:37 +0200

    Commit
    Created Package

--- Package.swift
+++ Package.swift
@@ -1,28 +1,39 @@
-// swift-tools-version: 5.6
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version:5.6
 
 import PackageDescription
 
-let package = Package(
-    name: "swift-numerals",
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "swift-numerals",
-            targets: ["swift-numerals"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "swift-numerals",
-            dependencies: []),
-        .testTarget(
-            name: "swift-numeralsTests",
-            dependencies: ["swift-numerals"]),
-    ]
+let package: Package = .init(
+	name: "swift-numerals",
+	defaultLocalization: "en",
+	platforms: [
+		.iOS(.v13),
+		.macCatalyst(.v15),
+		.macOS(.v10_15),
+		.watchOS(.v6)
+	],
+	products: [
+		.library(name: "Numerals", targets: ["Numerals"])
+	],
+	dependencies: [
+		.package(url: "https://github.com/alexandrehsaad/swift-numeric-protocols.git", branch: "main"),
+		.package(url: "https://github.com/apple/swift-docc-plugin.git", .upToNextMinor(from: "1.0.0"))
+	],
+	targets: [
+		.target(
+			name: "Numerals",
+			dependencies: [
+				.product(name: "NumericProtocols", package: "swift-numeric-protocols")
+			],
+			path: "Sources"
+		),
+		.testTarget(
+			name: "MeasuresTests",
+			dependencies: ["Numerals"],
+			path: "Tests",
+			resources: [
+				.process("MeasuresTests/Resources")
+			]
+		)
+	],
+	swiftLanguageVersions: [.v5]
 )
